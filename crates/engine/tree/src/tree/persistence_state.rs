0026c85423fdfed6eb452d40932b532b85eb2c35
Keefe Liu <keefe.l@users.noreply.github.com>
2024-11-15 09:38:22 +0800

    feat: calculate state root in background

--- crates/engine/tree/src/tree/persistence_state.rs
+++ crates/engine/tree/src/tree/persistence_state.rs
@@ -1,5 +1,6 @@
 use alloy_eips::BlockNumHash;
 use alloy_primitives::B256;
+use reth_trie::updates::TrieUpdates;
 use std::{collections::VecDeque, time::Instant};
 use tokio::sync::oneshot;
 use tracing::{debug, trace};
@@ -14,6 +15,12 @@ pub struct PersistenceState {
     /// Receiver end of channel where the result of the persistence task will be
     /// sent when done. A None value means there's no persistence task in progress.
     pub(crate) rx: Option<(oneshot::Receiver<Option<BlockNumHash>>, Instant)>,
+
+    /// Receiver end of channel where the result of the persistence task will be
+    /// sent when done. A None value means there's no persistence task in progress.
+    #[allow(clippy::type_complexity)]
+    pub(crate) rx_with_root_updates:
+        Option<(oneshot::Receiver<(Option<BlockNumHash>, TrieUpdates)>, Instant)>,
     /// The block above which blocks should be removed from disk, because there has been an on disk
     /// reorg.
     pub(crate) remove_above_state: VecDeque<u64>,
@@ -23,12 +30,20 @@ impl PersistenceState {
     /// Determines if there is a persistence task in progress by checking if the
     /// receiver is set.
     pub(crate) const fn in_progress(&self) -> bool {
-        self.rx.is_some()
+        self.rx.is_some() || self.rx_with_root_updates.is_some()
     }
 
     /// Sets state for a started persistence task.
     pub(crate) fn start(&mut self, rx: oneshot::Receiver<Option<BlockNumHash>>) {
         self.rx = Some((rx, Instant::now()));
+    }
+
+    /// Sets state for a started persistence task with root updates.
+    pub(crate) fn start_with_root_update(
+        &mut self,
+        rx: oneshot::Receiver<(Option<BlockNumHash>, TrieUpdates)>,
+    ) {
+        self.rx_with_root_updates = Some((rx, Instant::now()));
     }
 
     /// Sets the `remove_above_state`, to the new tip number specified, only if it is less than the
@@ -46,6 +61,7 @@ impl PersistenceState {
     ) {
         trace!(target: "engine::tree", block= %last_persisted_block_number, hash=%last_persisted_block_hash, "updating persistence state");
         self.rx = None;
+        self.rx_with_root_updates = None;
         self.last_persisted_block =
             BlockNumHash::new(last_persisted_block_number, last_persisted_block_hash);
     }
